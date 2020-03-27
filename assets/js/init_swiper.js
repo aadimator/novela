e82434a369498f4c623defe82fea406a37000e34
Aadam <aadimator@gmail.com>
2020-03-27 21:33:11 +0500

    feat: complete certificates slider, hero section

--- assets/js/init_swiper.js
+++ assets/js/init_swiper.js
@@ -1,11 +1,23 @@
-var mySwiper = new Swiper(".swiper-container", {
+var mySwiper = new Swiper(".swiper1", {
   slidesPerView: 1,
   centeredSlides: true,
   grabCursor: true,
   spaceBetween: 30,
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper1-pagination",
+    clickable: true
+  }
+});
+
+var certSwiper = new Swiper(".swiper2", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  grabCursor: true,
+  spaceBetween: 30,
+  // If we need pagination
+  pagination: {
+    el: ".swiper2-pagination",
     clickable: true
   }
 });
